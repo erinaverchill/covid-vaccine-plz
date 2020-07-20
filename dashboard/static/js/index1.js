cc0bb4cd979472853e9a84ab3d89b0b4539e1913
Srinivas Hanumansetty <DRSetty@SETE-CHEM-4.local>
2020-07-20 05:40:06 -0700

    index files updated with graphs

---
+++ dashboard/static/js/index1.js
@@ -0,0 +1,101 @@
+function makeChart(players) {
+
+    var cityLabels = players.map(function(d) {
+        return d.city;
+    });
+    var totalcountconfirmedData = players.map(function(d) {
+        return +d.totalcountconfirmed;
+    });
+
+    var totalcountdeathsData = players.map(function(d) {
+        return +d.totalcountdeaths;
+    });
+
+    var chart = new Chart('chart', {
+        type: "horizontalBar",
+        options: {
+            maintainAspectRatio: false,
+            legend: {
+                display: false
+            }
+        },
+        data: {
+            labels: cityLabels,
+            datasets: [
+
+                {
+                    backgroundColor: 'rgba(247, 163, 91, 1)',
+                    borderColor:  'rgba(247, 163, 91, 1)',
+                    borderWidth: 1,
+                    data: totalcountconfirmedData,
+
+                },
+                {
+                    backgroundColor: 'rgba(144, 238, 125, 1)',
+                    borderColor: 'rgba(144, 238, 125, 1)',
+                    borderWidth: 1,
+                    data: totalcountdeathsData,
+
+                }
+            ]
+        }
+    });
+}
+
+// Request data using D3
+d3
+    .csv("citywide.csv").then(function(data) {
+        makeChart(data);
+});
+
+
+
+function makeChart_two(players) {
+
+    var countyLabels = players.map(function(d) {
+        return d.county;
+    });
+    var totalcountconfirmedData = players.map(function(d) {
+        return +d.totalcountconfirmed;
+    });
+
+    var totalcountdeathsData = players.map(function(d) {
+        return +d.totalcountdeaths;
+    });
+
+    var chart = new Chart('chart_two', {
+        type: "line",
+        options: {
+            maintainAspectRatio: true,
+            legend: {
+                display: false
+            }
+        },
+        data: {
+            labels: countyLabels,
+            datasets: [
+
+                {
+                    backgroundColor: 'rgba(247, 163, 91, 1)',
+                    borderColor:  'rgba(247, 163, 91, 1)',
+                    borderWidth: 1,
+                    data: totalcountconfirmedData,
+
+                },
+                {
+                    backgroundColor: 'rgba(144, 238, 125, 1)',
+                    borderColor: 'rgba(144, 238, 125, 1)',
+                    borderWidth: 1,
+                    data: totalcountdeathsData,
+
+                }
+            ]
+        }
+    });
+}
+
+// Request data using D3
+d3
+    .csv("statewide.csv").then(function(data) {
+    makeChart_two(data);
+});
